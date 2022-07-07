29da9b5bdf5bbc10748968e319da418326c01e8b
momen kamal <momenkamal221@gmail.com>
2022-07-07 15:02:59 +0200

    expenses-chart-component

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,7 +1,5 @@
 const Column = ({spending,highestAmount}) => {
 	const height=`${(150*spending.amount)/highestAmount}px`;
-
-	console.log('here')
 	return (
 		<div className="expenses-card__chart" >
 			<div className={`expenses-card__chart-colum ${spending.highest?`expenses-card__chart-colum_max`:''}`}  style={{height:height}}></div>
